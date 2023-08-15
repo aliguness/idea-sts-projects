fbe10586f1ae66d33a1ac3a399880dbfb4fde077
aliguness <aligunescomu@gmail.com>
2023-08-15 13:39:03 +0300

    Docker

---
+++ _083_Docker001-hello/src/main/java/com/aligunes/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.aligunes.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+
+        return "hello Docker";
+    }
+}
