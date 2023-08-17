b7abd4c0c90f3e10e2078689d1790e8da65b20be
aliguness <aligunescomu@gmail.com>
2023-08-17 16:50:48 +0300

    Docker
    Ornek002

--- _084_Docker_Versionlar/src/main/java/com/aligunes/controller/HelloController.java
+++ _084_Docker_Versionlar/src/main/java/com/aligunes/controller/HelloController.java
@@ -3,7 +3,6 @@ package com.aligunes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 // http://localhost:8080/api/v1
@@ -18,4 +17,15 @@ public class HelloController {
     public String hello(){
         return "hello Docker";
     }
+
+    @GetMapping("/metod2")
+    public String metod2(){
+        return "Metod 2-Ali";
+    }
+
+    @GetMapping("/metod3")
+    public String meto3(){
+        return "Metod 3- Ali Güneş";
+    }
+
 }
