46949c75570bcf84ee249e695a5d5797dbcc0992
aliguness <aligunescomu@gmail.com>
2023-09-16 22:39:46 +0300

    Microservice

--- SocialMediaMyself/auth-service/src/main/java/com/aligunes/exception/AuthManagerException.java
+++ SocialMediaMyself/auth-service/src/main/java/com/aligunes/exception/AuthManagerException.java
@@ -1,13 +1,23 @@
 package com.aligunes.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class AuthManagerException extends RuntimeException{
 
-    private final ErrorType errorType;
-    public AuthManagerException(ErrorType errorType){
-        this.errorType=errorType;
-    }
+    private final ErrorType errorType; //1
+
+    private final String customMessage; //2
+
+//    public AuthManagerException(ErrorType errorType){
+//        this.errorType=errorType;
+//    }
+//
+//    public AuthManagerException(ErrorType errorType, String customMessage){
+//        super(customMessage);
+//        this.errorType=errorType;
+//    }
 
 }
--- SocialMediaMyself/auth-service/src/main/java/com/aligunes/repository/entity/Auth.java
+++ SocialMediaMyself/auth-service/src/main/java/com/aligunes/repository/entity/Auth.java
@@ -19,8 +19,10 @@ public class Auth extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String username;
     private String password;
+    @Column(unique = true)
     private String email;
     private String activationCode;
     @Enumerated(EnumType.STRING)
